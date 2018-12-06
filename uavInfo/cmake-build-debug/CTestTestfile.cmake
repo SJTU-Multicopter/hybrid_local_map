3f63bd3b40f14c7bc2a416f73b52d2adf6613a35
g-ch <947089399@qq.com>
2018-12-06 18:56:43 +0800

    delt_yaw +- Pi, current_yaw_x_y

--- uavInfo/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/catkin_ws/src/hybrid_local_map/uavInfo
-# Build directory: /home/ubuntu/catkin_ws/src/hybrid_local_map/uavInfo/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
