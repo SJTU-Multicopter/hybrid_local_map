0f84136b846e4ca95158d7a4b252657e646025e1
g-ch <947089399@qq.com>
2019-12-04 16:02:20 +0800

    add cost displsplay panner

--- secure_flying/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cc/catkin_ws/src/hybrid_local_map/secure_flying
-# Build directory: /home/cc/catkin_ws/src/hybrid_local_map/secure_flying/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
